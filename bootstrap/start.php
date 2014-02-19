c9362c9670a2639d1c9c1744e28c4d4148bd151d
Devin Smith <windwardrail@gmail.com>
2014-02-19 01:46:27 -0800

    fixed env detect fx

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -24,19 +24,10 @@ $app = new Illuminate\Foundation\Application;
 |
 */
 
-$env = $app->detectEnvironment(array(
-
-	'local' => array('your-machine-name'),
-
-
-
-));
-
-
 $env = $app->detectEnvironment(function () {
-    return isset($_SERVER['LARAVEL_ENV'])
-        ? $_SERVER['LARAVEL_ENV']
-        : 'development'; // or whatever fallback you prefer
+    return isset($_SERVER['LARAVEL_ENV']) ? $_SERVER['LARAVEL_ENV'] : 'development'; // or whatever fallback you prefer
+  });
+
 
 /*
 |--------------------------------------------------------------------------
