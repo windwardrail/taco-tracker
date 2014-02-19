94b2968e75203dff75427698d054f1395e856bf2
Devin Smith <windwardrail@gmail.com>
2014-02-18 23:57:12 -0800

    tweaks

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,8 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('your-machine-name'),
+	'local' => array('localhost'),
+  'production' => array('taco-tracker.gopagoda.com')
 
 ));
 
