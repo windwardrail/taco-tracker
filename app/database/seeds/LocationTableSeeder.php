29ca1cae1ddce4719c3603178f1c6c92909010a9
Devin Smith <windwardrail@gmail.com>
2014-02-19 02:36:17 -0800

    modified seed data truck location

--- app/database/seeds/LocationTableSeeder.php
+++ app/database/seeds/LocationTableSeeder.php
@@ -10,7 +10,7 @@ class LocationTableSeeder extends Seeder {
     $locations = array(
         ['latitude' => '48.749543', 'longitude' => '-122.473016', 'truck_id' => 1],
         ['latitude' => '48.760250', 'longitude' => '-122.464707', 'truck_id' => 2],
-        ['latitude' => '48.766770', 'longitude' => '-122.485810', 'truck_id' => 3]
+        ['latitude' => '48.757037', 'longitude' => '-122.476802', 'truck_id' => 3]
     );
 
     // Uncomment the below to run the seeder
