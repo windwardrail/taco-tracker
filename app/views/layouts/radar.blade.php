3d93c42565a7377faf03dbbfba838d3feae53c06
Devin Smith <windwardrail@gmail.com>
2014-02-19 02:30:29 -0800

    troubleshooting map

---
+++ app/views/layouts/radar.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <style type="text/css">
+      html { height: 100% }
+      body { height: 100%; margin: 0; padding: 0 }
+      #map-canvas { height: 100% }
+    </style>
+    <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAtOxrot7xZqWgX7OI6fjgljtI4_BPCEMA&sensor=false"></script>
+  </head>
+
+  <body>
+    @yield('main')
+  </body>
+
+</html>
--- app/views/tacos/index.blade.php
+++ app/views/tacos/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.scaffold')
+@extends('layouts.radar')
 
 @section('main')
 
